135aaf1d3add92b2a5de0aa907912f3a4a4c3cb0
DattaprasadTambe <76653504+DattaprasadTambe@users.noreply.github.com>
2022-07-29 22:32:09 +0530

    Add files via upload

---
+++ July22/DictionaryDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProrigoTraining.July22
+{
+    internal class DictionaryDemo
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dd = new Dictionary<int, string>();
+            dd.Add(1, "Datta");
+            dd.Add(2, "Prasad");
+            dd.Add(3,"Subhash");
+            dd.Add(4, "Tambe");
+
+            dd[3] = "DP";
+
+            dd.Remove(4);
+
+            Console.WriteLine("Count of dictionary: "+dd.Count);
+
+            Console.WriteLine(dd.ContainsKey(5));
+            Console.WriteLine(dd.ContainsValue("DP"));
+
+            List<int> k = dd.Keys.ToList();
+
+           Console.WriteLine(dd[3]);
+
+            foreach(KeyValuePair<int, string> kv in dd)
+            {
+                Console.WriteLine(kv.Key+"   "+kv.Value);
+            }
+        }
+    }
+
+   
+}
---
+++ July22/LinkedListDemo1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProrigoTraining.July22
+{
+   
+    class LinkedListDemo
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> l1= new LinkedList<int>();
+            l1.AddLast(1000);
+            l1.AddLast(2000);
+            l1.AddFirst(3000);
+        }
+    }
+
+    internal class LinkedListDemo1
+    {
+        public static void AddLinkedList(LinkedList<int> ll)
+        {
+            Console.WriteLine("Enter elements: ");
+            ll.AddLast(int.Parse(Console.ReadLine()));
+            ll.AddLast(int.Parse(Console.ReadLine()));
+            ll.AddFirst(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Add element after: ");
+            ll.AddAfter(ll.Find(int.Parse(Console.ReadLine())), int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Add element before: ");
+            ll.AddBefore(ll.Find(int.Parse(Console.ReadLine())), int.Parse(Console.ReadLine()));
+            foreach (int i in ll)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            LinkedList<int> ll = new LinkedList<int>();
+            AddLinkedList(ll);
+
+            
+        }
+    }
+}
